69395a8eb4f7e95c54064d63200ed8714f0179bb
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-02-27 14:40:24 +0200

    2 task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const player = new Player('vimeo-player', {
+  id: 19231868,
+  width: 640,
+});
 
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const pausedTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(pausedTime);
