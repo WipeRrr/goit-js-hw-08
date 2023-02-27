65a6e0a9ec13f646151f162bbc1ed25be32c18ad
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-02-27 13:27:58 +0200

    1 task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const paletteContainer = document.querySelector(`.gallery`);
+const imgGallery = createImgGallery(galleryItems);
+
+function createImgGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image"   loading="lazy" src="${preview}" alt="${description}" />
+</a>
+`;
+    })
+    .join('');
+}
+
+paletteContainer.insertAdjacentHTML(`beforeend`, imgGallery);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
