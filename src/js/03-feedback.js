5678ae61cdfdc97fd506502723a45f5c8ac87f21
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-03-02 01:25:56 +0200

    3 task done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,27 +1,32 @@
-import throttle from 'lodash.throttle';
+import throttled from 'lodash.throttle';
 
 const FEEDBACK_FORM_STATE = 'feedback-form-state';
-const inputRef = document.querySelector('.feedback-form');
+const formInputRef = document.querySelector('.feedback-form');
+const throttledChange = throttled(onImputChange, 500);
 
-inputRef.addEventListener(`input`, onImputChange);
-inputRef.addEventListener(`submit`, onSubmit);
+formInputRef.addEventListener('input', throttledChange);
+formInputRef.addEventListener(`submit`, onSubmit);
 
 populateData();
-function onImputChange(evt) {
-  const email = evt.currentTarget.elements.email.value;
 
-  const message = evt.currentTarget.elements.message.value;
-  const formData = {
-    email,
-    message,
-  };
+const formData = {
+  email: '',
+  message: '',
+};
+
+function onImputChange(evt) {
+  if (evt.target.nodeName == 'INPUT') {
+    formData.email = evt.target.value;
+  } else if (evt.target.nodeName == 'TEXTAREA') {
+    formData.message = evt.target.value;
+  }
 
   localStorage.setItem(FEEDBACK_FORM_STATE, JSON.stringify(formData));
 }
 
 function onSubmit(evt) {
   evt.preventDefault();
-  console.log(inputRef.email.value, inputRef.message.value);
+  console.log(JSON.parse(localStorage.getItem(FEEDBACK_FORM_STATE)));
   evt.currentTarget.reset();
   localStorage.removeItem(FEEDBACK_FORM_STATE);
 }
@@ -29,10 +34,12 @@ function onSubmit(evt) {
 function populateData() {
   const savedData = localStorage.getItem(FEEDBACK_FORM_STATE);
   const parsedSavedData = JSON.parse(savedData);
+  console.log(parsedSavedData);
+  console.log(savedData);
   if (savedData) {
     console.log(parsedSavedData.email);
     console.log(parsedSavedData.message);
-    inputRef.email.value = parsedSavedData.email;
-    inputRef.message.value = parsedSavedData.message;
+    formInputRef.email.value = parsedSavedData.email;
+    formInputRef.message.value = parsedSavedData.message;
   }
 }
